261686c934263b09075811acb898702c2b49f228
Sushank Gurung <jagatgurung30@gmail.com>
2025-03-04 14:46:56 +0545

    expose api

--- src/main.js
+++ src/main.js
@@ -5,13 +5,15 @@ import getAllInputData from "./modules/inputData.js";
 import getExchangeRates from "./modules/getExchangeRates.js";
 import displayExchangeRates from "./modules/displayEchangeRate.js";
 
-window.addEventListener("load", async () => {
+const onLoad = async () => {
   const today = new Date();
   const formattedDate = today.toISOString().split("T")[0];
   document.getElementById("date-selector").value = formattedDate;
 
   await dropDown();
-});
+};
+
+onLoad();
 
 const convert = document.getElementById("convert");
 
